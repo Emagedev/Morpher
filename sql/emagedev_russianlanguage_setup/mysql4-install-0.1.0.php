8679cc2013551ea800253799e32927193cab95c9
Dmitry Burlakov <dantaeusb@live.com>
2015-11-23 00:16:20 +0300

    OMR-79 Inflection engine

---
+++ sql/emagedev_russianlanguage_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Emagedev extension for Magento
+ * 
+ * Add table for custom menu
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * 
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the VF CustomMenu module to newer versions in the future.
+ * If you wish to customize the VF CustomMenu module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Emagedev
+ * @package    Emagedev_RussianLanguage
+ * @copyright  Copyright (C) 2012 Vladimir Fishchenko (http://fishchenko.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * @var $this Mage_Core_Model_Resource_Setup
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$installer->run("
+CREATE TABLE {$installer->getTable('emagedev_russian/inflections')} (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `phrase` text NOT NULL,
+  `inflection` VARCHAR(1) NOT NULL,
+  `multi` TINYINT NOT NULL DEFAULT 0,
+  `inflected_phrase` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+");
+
+$installer->endSetup();
